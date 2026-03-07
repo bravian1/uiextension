a7d36f549b37cdca08f2addd99d90bf840e1f8c7
bravian1 <nyatorobravian@gmail.com>
2026-03-07 08:32:31 +0300

    chore: setup project configuration, vite, and tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          400: '#a78bfa',
+          500: '#8b5cf6',
+          600: '#7c3aed',
+        },
+        dark: {
+          600: '#4b5563',
+          700: '#374151',
+          800: '#1f2937',
+          900: '#111827',
+        }
+      }
+    },
+  },
+  plugins: [],
+}
